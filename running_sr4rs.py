a67c98750737d624910477a3522c330ea02b9533
rianmrwn <134907903+rianmrwn@users.noreply.github.com>
2025-02-28 14:19:20 +0700

    Update running_sr4rs.py

--- running_sr4rs.py
+++ running_sr4rs.py
@@ -1,21 +1,12 @@
 import argparse
 import subprocess
 
-def run_docker(device):
-    if device == 'gpu':
-        subprocess.run(['docker', 'run', '-ti', '--runtime=nvidia', 'mdl4eo/otbtf:3.4.0-gpu', 'bash'])
-    else:
-        subprocess.run(['docker', 'run', '-ti', '-v', '~/data:/mnt/data', 'mdl4eo/otbtf:3.4.0-cpu', 'bash'])
-
 def download_and_unzip_model():
     subprocess.run(['wget', 'https://nextcloud.inrae.fr/s/boabW9yCjdpLPGX/download/sr4rs_sentinel2_bands4328_france2020_savedmodel.zip'])
     subprocess.run(['unzip', 'sr4rs_sentinel2_bands4328_france2020_savedmodel.zip'])
 
-def clone_repo(git_url):
-    subprocess.run(['git', 'clone', git_url])
-
 def run_sr_script(input_file, output_file):
-    subprocess.run(['python', 'sr4rs/code/sr.py', '--savedmodel', 'sr4rs_sentinel2_bands4328_france2020_savedmodel', '--input', input_file, '--output', output_file])
+    subprocess.run(['python', 'SR4RS_Auto/code/sr.py', '--savedmodel', 'sr4rs_sentinel2_bands4328_france2020_savedmodel', '--input', input_file, '--output', output_file])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Automate Docker and Python workflow')
